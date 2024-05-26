f9034c48a25b87271b10e3fdc693e949617c00cc
armsves <armsves@gmail.com>
2024-05-26 09:55:02 +0200

    adding social media and updating readme

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,6 +7,8 @@ import { usePathname } from "next/navigation";
 import { useRouter } from "next/navigation";
 import { ReactEventHandler } from "react";
 import InlineSVG from "react-inlinesvg";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 
 const Footer = () => {
@@ -20,9 +22,17 @@ const Footer = () => {
     const { isClosed } = constants;
 
     return (
-      <footer className="fixed bottom-0 left-0 flex w-full items-end justify-center bg-primary h-10 py-2 text-white">
-          Powered by dRPC
-      </footer>
+<footer className="fixed bottom-0 left-0 flex w-full items-center justify-between bg-primary h-10 py-2 text-white px-4">
+    <a href="https://github.com/armsves/ethBerlin04/" style={{marginLeft: '20%'}} target="_blank">
+        <FontAwesomeIcon icon={faGithub} />
+    </a>
+    <div>
+        Powered by dRPC
+    </div>
+    <a href="https://twitter.com/armsves" style={{marginRight: '20%'}} target="_blank">
+        <FontAwesomeIcon icon={faTwitter} />
+    </a>
+</footer>
     )
   };
 
